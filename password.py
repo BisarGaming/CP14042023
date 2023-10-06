ffba854b555a173d3166b038962ef654ba78aa5f
Oliver Bisaro <oliver.bisaro@my.jcu.edu.au>
2023-10-07 01:37:16 +1000

    working password checker

---
+++ password.py
@@ -0,0 +1,23 @@
+""" Password Stars"""
+
+
+def main():
+    minimum_length = int(input("Minimum length: "))
+    password = get_password(minimum_length)
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    for i in range(len(password)):
+        print("*", end="")
+
+
+def get_password(minimum_length):
+    password = input("Password: ")
+    while len(password) < minimum_length:
+        print("Invalid")
+        password = input("Password: ")
+    return password
+
+
+main()
