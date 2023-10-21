3361859eebc23a481cdeed99e64e9464712fd37e
Oliver Bisaro <oliver.bisaro@my.jcu.edu.au>
2023-10-21 16:46:02 +1000

    working password checker working temperature converter (favourite prac so far) working score
    String Formatting prac 3
    reminder for later to sort out why git projects aren't in folders
    
    solved questions for randoms and uploaded randoms
    
    capitalist_conrad.py was added
    
    excetions_demo was added
    answers underneath code.
    
    exceptions to compltete addded
    
    files with name and numbers text files added
    
    IMPORTANT - Organised into directories (should be correctly formatted)
    
    added prac 4 list warmup
    
    used starter code and then corrected it
    
    subject reader and data added, able to add to the text file
    
    completed the comprehensions and excersises for the lists

---
+++ prac 4/list_exercises.py
@@ -0,0 +1,34 @@
+# 1. Basic list operations
+NUMBER_OF_NUMBERS = 5
+
+
+def main():
+    numbers = []
+    for i in range(NUMBER_OF_NUMBERS):
+        number = int(input("Number: "))
+        numbers.append(number)
+    print_interesting_statements(numbers)
+
+
+def print_interesting_statements(numbers):
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The average of the number is {calculate_average(numbers)}")
+
+
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers)
+
+
+main()
+
+# 2. Woefully inadequate security checker (Woefully is a weird word)
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
